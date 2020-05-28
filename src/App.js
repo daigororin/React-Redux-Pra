96cb168cb25a1b70caad196ce91a96f6a6274fab
daigororin <60973962+daigororin@users.noreply.github.com>
2020-05-29 05:56:55 +0900

    importPropTypes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
+// import { render } from '@testing-library/react';
+import propTypes from 'prop-types';
 
-function App() {
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">BAR</label>
+//       <input type="box" onClick={() => { console.log("Clicked") }} />
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
+  const profiles = [
+    { name: 'Taro', age: 20 },
+    { name: 'Hanako', age: 15 },
+    { name: 'Yuko', age: 15 },
+    { name: 'Takashi', age: 3 },
+  ]
   return (
-    <React.Fragment>
-      <label htmlFor="bar">BAR</label>
-      <input type="box" onClick={() => { console.log("Clicked") }} />
-    </React.Fragment>
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+    </div>
   );
+}
+
+const User = (props) => {
+  return <div>Hi! I'm {props.name}! and {props.age} years old.</div>
+}
+
+User.propTypes = {
+  name: propTypes.string,
+  age: propTypes.number.isRequired
 }
 
 export default App;
