c7585b7ae096f62fa38f94309b839fb9f93ac397
daigororin <60973962+daigororin@users.noreply.github.com>
2020-05-30 01:13:56 +0900

    fixedCounter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
-// import { render } from '@testing-library/react';
-// import PropTypes from 'prop-types';
 
-// function App() {
-//   return (
-//     <React.Fragment>
-//       <label htmlFor="bar">BAR</label>
-//       <input type="box" onClick={() => { console.log("Clicked") }} />
-//     </React.Fragment>
-//   );
-// }
-
-const App = () => (<Counter></Counter>)
+const App = () => (<Counter />)
 
 class Counter extends Component {
   constructor(props) {
@@ -22,18 +11,15 @@ class Counter extends Component {
 
   handlePlusButton = () => {
     this.setState({ count: this.state.count + 1 })
-    console.log(this.state.count)
   }
 
   handleMinusButton = () => {
     if (this.state.count > 0) {
       this.setState({ count: this.state.count - 1 })
     }
-    console.log(this.state.count)
   }
 
   render() {
-    {/* console.log(this.state.count) */ }
     return (
       <>
         <div>Counter:{this.state.count}</div>
